54f451690cacd8dee27dadde4bf742f8f40b6d58
amazingproducer <amazingproducer@gmail.com>
2021-01-29 09:57:55 +0000

    stretch iv complete

--- assignments-iv/lambda-classes.js
+++ assignments-iv/lambda-classes.js
@@ -25,6 +25,17 @@ class Instructor extends Person {
     grade(student, subject) {
         return `${student.name} receives a perfect score on ${subject}`;
     }
+    assess(student) {
+        this.maxFail = -10;
+        this.maxPass = 10;
+        if(!student.grade) {
+            student.grade = 0;
+        }
+        student.lastAssessmentValue = Math.floor(Math.random() * (this.maxPass - this.maxFail + 1) + this.maxFail);
+        student.previousGrade = student.grade;
+        student.grade = student.previousGrade + student.lastAssessmentValue;
+        return `Grade reassessed for ${student.name} by ${this.name}: ${student.previousGrade}->${student.grade} (${student.lastAssessmentValue}). ${student.graduate()}`
+    }
 }
 
 class Student extends Person {
@@ -33,6 +44,7 @@ class Student extends Person {
         this.previousBackground = studentAttrs.previousBackground;
         this.className = studentAttrs.className;
         this.favSubjects = studentAttrs.favSubjects;
+        this.grade = studentAttrs.grade;
     }
     listSubjects() {
         // list student's subjects, one per line, in log
@@ -45,6 +57,13 @@ class Student extends Person {
     };
     sprintChallenge(subject) {
         return `${this.name} has submitted a PR for ${subject}`;
+    };
+    graduate() {
+        if(this.grade > 70) {
+            return `${this.name} is eligible to graduate.`;
+        } else {
+            return `${this.grade} is too low of a grade for ${this.name} to graduate.`;
+        }
     };
 }
 
@@ -65,6 +84,7 @@ class ProjectManager extends Instructor {
 const harry = new Student({
     name: "harry",
     favSubjects: ["spells", "potions", "husbandry"],
+//    grade: Math.floor(Math.random() * 101),
     previousBackground: "closet squatter",
 });
 
@@ -73,6 +93,25 @@ const barry = new ProjectManager({
     favInstructor: "Life itself",
 })
 
-console.log(harry.favSubjects.length);
-console.log(harry.listSubjects());
-console.log(barry.debugsCode(harry, "self driving cars"))
+console.log(harry.grade);
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
+console.log(barry.assess(harry));
